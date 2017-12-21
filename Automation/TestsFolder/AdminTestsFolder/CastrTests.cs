f677a8bf149c6079600400596d0a99c7a063a538
itaymelamed <itaymelamed@ymail.com>
2017-12-21 15:05:45 +0200

    test

--- Automation/TestsFolder/AdminTestsFolder/CastrTests.cs
+++ Automation/TestsFolder/AdminTestsFolder/CastrTests.cs
@@ -27,25 +27,25 @@ namespace Automation.TestsFolder.AdminTestsFolder
             }
         }
 
-        [TestFixture]
-        [Parallelizable]
-        public class Test2Class : Base
-        {
-            [Test]
-            [Property("TestCaseId", "15")]
-            [Category("Sanity")][Category("Admin")][Category("Castr")]
-            public void Caster_NavigateToCastrFromAdminPage()
-            {
-                HomePage homePage = new HomePage(_browser);
-                FaceBookconnectPage faceBookconnectPage = homePage.ClickOnConnectBtn();
-                HomePage homePageConnected = faceBookconnectPage.Login(_config.ConfigObject.Users.AdminUser);
-                homePageConnected.ValidateUserProfilePic();
-                homePageConnected.HoverOverUserProfilePic();
-                AdminPage adminPage = homePageConnected.ClickOnAdmin();
-                CastrPage castrPage = adminPage.ClickOnCasterLink();
+        //[TestFixture]
+        //[Parallelizable]
+        //public class Test2Class : Base
+        //{
+        //    [Test]
+        //    [Property("TestCaseId", "15")]
+        //    [Category("Sanity")][Category("Admin")][Category("Castr")]
+        //    public void Caster_NavigateToCastrFromAdminPage()
+        //    {
+        //        HomePage homePage = new HomePage(_browser);
+        //        FaceBookconnectPage faceBookconnectPage = homePage.ClickOnConnectBtn();
+        //        HomePage homePageConnected = faceBookconnectPage.Login(_config.ConfigObject.Users.AdminUser);
+        //        homePageConnected.ValidateUserProfilePic();
+        //        homePageConnected.HoverOverUserProfilePic();
+        //        AdminPage adminPage = homePageConnected.ClickOnAdmin();
+        //        CastrPage castrPage = adminPage.ClickOnCasterLink();
 
-                Assert.True(castrPage.ValidateCasterPage());
-            }
-        }
+        //        Assert.True(castrPage.ValidateCasterPage());
+        //    }
+        //}
     }
 }
