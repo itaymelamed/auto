700bf0697c36db693c5165eca08bd1616dc06a9e
itaymelamed <itaymelamed@ymail.com>
2017-12-26 16:36:14 +0200

    test

--- Automation/TestsObject/Test.cs
+++ Automation/TestsObject/Test.cs
@@ -28,7 +28,8 @@ namespace Automation.TestsObjects
             Result = new Result(TestContext.CurrentContext.Result, TestStatus.SentToHub);
             TestNumber = TestContext.CurrentContext.Test.Properties.Get("TestCaseId").ToString();
             TestRunId = Base._testRun.TestRunId;
-            TestName = string.Concat(TestContext.CurrentContext.Test.Name.Select(x => Char.IsUpper(x) ? " " + x : x.ToString())).TrimStart(' ');
+            TestName = string.Concat(TestContext.CurrentContext.Test.Name.Select(x => Char.IsUpper(x) ? " " + x : x.ToString())).TrimStart(' ').
+                             Replace('-', '|');
             Steps = new List<string>();
             EnvironmentType = config.Env.ToString();
             Date = DateTime.Now.ToString("dd/MM/yyyy H:mm");
