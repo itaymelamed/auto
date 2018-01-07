c70112d83534a2d3ed1cc774b63146bfce88f879
itaymelamed <itaymelamed@ymail.com>
2018-01-07 18:09:40 +0200

    test

--- Automation/BrowserFolder/HubLoadBalancer.cs
+++ Automation/BrowserFolder/HubLoadBalancer.cs
@@ -23,13 +23,10 @@ namespace Automation.BrowserFolder
 
         public string GetAvalibleHub()
         {
-            lock(_syncObject)
-            {
-                if (int.Parse(_hub1) >= int.Parse(_hub2))
-                    return $"http://{_config.GetIp(0)}:4444/wd/hub";
-                else
-                    return $"http://{_config.GetIp(1)}:4444/wd/hub";
-            }
+            if (int.Parse(_hub1) >= int.Parse(_hub2))
+                return $"http://{_config.GetIp(1)}:4444/wd/hub";
+            else
+                return $"http://{_config.GetIp(1)}:4444/wd/hub";
         }
     }
 }
