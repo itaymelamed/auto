2d09fd17f9eefdeb666a47d2b3e8c55e13ed7018
itaymelamed <itaymelamed@ymail.com>
2018-01-10 13:43:40 +0200

    throe ex

--- Automation/TestsFolder/Base.cs
+++ Automation/TestsFolder/Base.cs
@@ -25,21 +25,28 @@ namespace Automation.TestsFolder
         [SetUp]
         public void InitTest()
         {
-            lock (_syncObject)
+            try
             {
-                _config = _config ?? new Configurations();
-                MongoDb = MongoDb ?? new MongoDb("TestRuns");
-                _testRun = _testRun ?? new TestRun(_config);
-            }
+                lock (_syncObject)
+                {
+                    _config = _config ?? new Configurations();
+                    MongoDb = MongoDb ?? new MongoDb("TestRuns");
+                    _testRun = _testRun ?? new TestRun(_config);
+                }
 
-            _test = new Test(_config);
-            _params = new Params(_test, _config).GetParams();
-            _test.UpdateTestStatus(TestContext.CurrentContext.Result, TestStatus.SentToHub);
-            _hubLoadBalancer = !_config.Local? new HubLoadBalancer(_config) : null;
-            _browser = !_config.Local ? new Browser(_hubLoadBalancer) : new Browser();
-            _test.UpdateTestStatus(TestContext.CurrentContext.Result, TestStatus.Running);
-            _browser.Maximize();
-            _browser.Navigate(_config.Url);
+                _test = new Test(_config);
+                _params = new Params(_test, _config).GetParams();
+                _test.UpdateTestStatus(TestContext.CurrentContext.Result, TestStatus.SentToHub);
+                _hubLoadBalancer = !_config.Local ? new HubLoadBalancer(_config) : null;
+                _browser = !_config.Local ? new Browser(_hubLoadBalancer) : new Browser();
+                _test.UpdateTestStatus(TestContext.CurrentContext.Result, TestStatus.Running);
+                _browser.Maximize();
+                _browser.Navigate(_config.Url);
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
         }
 
         [TearDown]
