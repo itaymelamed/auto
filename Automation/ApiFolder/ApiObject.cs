d862e480629e06a54fb6a954263d555b08914cd5
itaymelamed <itaymelamed@ymail.com>
2018-01-10 22:10:01 +0200

    added aPI PROXY SERVER

--- Automation/ApiFolder/ApiObject.cs
+++ Automation/ApiFolder/ApiObject.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using System.Collections.Specialized;
+using System.IO;
 using System.Net;
+using System.Text;
 using Newtonsoft.Json.Linq;
 
 namespace Automation.ApiFolder
@@ -13,6 +15,49 @@ namespace Automation.ApiFolder
                 var responseString = client.DownloadString(url);
                 return JObject.Parse(responseString);
             }
+        }
+
+        public WebResponse MakeRequest(string url, string method, string reference = null)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = method;
+            if (reference != null)
+            {
+                byte[] requestBytes = Encoding.UTF8.GetBytes(reference);
+                using (var requestStream = request.GetRequestStream())
+                {
+                    requestStream.Write(requestBytes, 0, requestBytes.Length);
+                    requestStream.Close();
+                }
+
+                request.ContentType = "application/x-www-form-urlencoded";
+            }
+            else
+                request.ContentLength = 0;
+
+            return request.GetResponse();
+        }
+
+        public WebResponse MakeJsonRequest(string url, string method, string payload)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = method;
+
+            if (payload != null)
+            {
+                request.ContentType = "text/json";
+                request.ContentLength = payload.Length;
+                using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+                {
+                    streamWriter.Write(payload);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+            }
+            else
+                request.ContentLength = 0;
+
+            return request.GetResponse();
         }
     }
 }
---
+++ Automation/ApiFolder/ProxyApiFolder/Har/ProxyApi.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using AutomatedTester.BrowserMob.HAR;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Automation.ApiFolder
+{
+    public class ProxyApi
+    {
+        readonly string _url;
+        readonly Int16 _port;
+        readonly string _proxy;
+        readonly string _baseUrlProxy;
+        readonly ApiObject _api;
+
+        public ProxyApi(string url)
+        {
+            _api = new ApiObject();
+            _url = url;
+            _baseUrlProxy = $"{_url}/proxy";
+            using (var response = _api.MakeRequest(_baseUrlProxy, "POST"))
+            {
+                var responseStream = response.GetResponseStream();
+                if (responseStream == null)
+                    throw new Exception("No response from proxy");
+
+                using (var responseStreamReader = new StreamReader(responseStream))
+                {
+                    var jsonReader = new JsonTextReader(responseStreamReader);
+                    var token = JToken.ReadFrom(jsonReader);
+                    var portToken = token.SelectToken("port");
+                    if (portToken == null)
+                        throw new Exception("No port number returned from proxy");
+
+                    _port = (Int16)portToken;
+                }
+            }
+
+            var parts = url.Split(':');
+            _proxy = parts[1].TrimStart('/') + ":" + _port;
+        }
+
+        public void NewHar(string reference = null)
+        {
+            _api.MakeRequest(String.Format("{0}/{1}/har", _baseUrlProxy, _port), "PUT", reference);
+        }
+
+
+        public void NewPage(string reference)
+        {
+            _api.MakeRequest(String.Format("{0}/{1}/har/pageRef", _baseUrlProxy, _port), "PUT", reference);
+        }
+
+        public HarResult GetHar()
+        {
+            var response = _api.MakeRequest($"{_baseUrlProxy}/{_port}/har", "GET");
+            using (var responseStream = response.GetResponseStream())
+            {
+                if (responseStream == null)
+                    return null;
+
+                using (var responseStreamReader = new StreamReader(responseStream))
+                {
+                    var json = responseStreamReader.ReadToEnd();
+                    return JsonConvert.DeserializeObject<HarResult>(json);
+                }
+            }
+        }
+
+        public string SeleniumProxy
+        {
+            get { return _proxy; }
+        }
+
+        static string FormatBlackOrWhiteListFormData(string regexp, int statusCode)
+        {
+            return String.Format("regex={0}&status={1}", HttpUtility.UrlEncode(regexp), statusCode);
+        }
+
+        public void Close()
+        {
+            _api.MakeRequest($"{_baseUrlProxy}/{_port}", "DELETE");
+        }
+    }
+}
