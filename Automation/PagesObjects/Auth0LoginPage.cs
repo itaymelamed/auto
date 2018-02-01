1402ec5d171eb2b9019f7c6ba6c39059737874c0
liorshag <lior3545>
2018-02-01 14:23:16 +0200

    test

---
+++ Automation/PagesObjects/Auth0LoginPage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Automation.BrowserFolder;
+using Automation.ConfigurationFoldee.ConfigurationsJsonObject;
+using Automation.TestsFolder;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace Automation.PagesObjects
+{
+    public class Auth0LoginPage
+    {
+        [FindsBy(How = How.CssSelector, Using = "[name='username']")]
+        IWebElement userNameTextBox { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "[name='password']")]
+        IWebElement passwordTextBox { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "[type='submit']")]
+        IWebElement loginBtn { get; set; }
+
+        protected Browser _browser;
+        protected IWebDriver _driver;
+        protected BrowserHelper _browserHelper;
+
+        public Auth0LoginPage(Browser browser)
+        {
+            _browser = browser;
+            _driver = browser.Driver;
+            _browserHelper = browser.BrowserHelper;
+            PageFactory.InitElements(_driver, this);
+        }
+
+
+        public NewsRoomPage Login(IUser user)
+        {
+            Base.MongoDb.UpdateSteps("Set Username");
+            _browserHelper.WaitForElement(userNameTextBox, nameof(userNameTextBox));
+            _browserHelper.SetText(userNameTextBox ,user.UserName);
+
+            Base.MongoDb.UpdateSteps("Set Password");
+            _browserHelper.WaitForElement(passwordTextBox, nameof(passwordTextBox));
+            _browserHelper.SetText(passwordTextBox, user.Password);
+
+            Base.MongoDb.UpdateSteps("Click on login BTN");
+            _browserHelper.WaitForElement(loginBtn, nameof(loginBtn));
+            _browserHelper.Click(loginBtn,nameof(loginBtn));
+
+            return new NewsRoomPage(_browser);
+        }
+    }
+}
---
+++ Automation/TestsFolder/AdminTestsFolder/NewsRoomTests.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Automation.PagesObjects;
+using MongoDB.Bson;
+using NUnit.Framework;
+
+namespace Automation.TestsFolder.AdminTestsFolder
+{
+    public class NewsRoomTests
+    {
+        [TestFixture]
+        [Parallelizable]
+        public class Test1Class : BaseUi
+        {
+            [Test]
+            [Property("TestCaseId", "14")]
+            [Category("Sanity")]
+            [Category("Admin")]
+            [Category("Auth0")]
+            [Category("MMNews")]
+            [Category("Pluralist")]
+            [Retry(2)]
+            public void Auth0_Login_Admin()
+            {
+                Auth0LoginPage loginPage = new Auth0LoginPage(_browser);
+                NewsRoomPage newsRoomPage = loginPage.Login(_config.ConfigObject.Users.AdminUser);
+                EditorPage editorPage = newsRoomPage.ClickOnEditorBtn();
+                Assert.True(editorPage.ValidateEditorTitle(), "Login failed");
+            }
+        }
+
+        [TestFixture]
+        [Parallelizable]
+        public class Test2Class : BaseUi
+        {
+            [Test]
+            [Property("TestCaseId", "56")]
+            [Category("Sanity")]
+            [Category("Admin")]
+            [Category("MMNews")]
+            [Category("FullFlow")]
+            [Category("Pluralist")]
+            [Retry(2)]
+            public void FullFlow_Article()
+            {
+                Auth0LoginPage loginPage = new Auth0LoginPage(_browser);
+                NewsRoomPage newsRoomPage = loginPage.Login(_config.ConfigObject.Users.AdminUser);
+                EditorPage editorPage = newsRoomPage.ClickOnEditorBtn();
+                BsonArray tagExValue = _params["Tags"].AsBsonArray;
+                string body = _params["Body"].ToString();
+                ArticleBase articleBase = editorPage.ClickOnArticle();
+                articleBase.WriteTitle("VIDEO:test article template");
+                CropImagePopUp cropImagePopUp = articleBase.DragImage(0);
+                cropImagePopUp.ClickOnCropImageBtn();
+                cropImagePopUp.ClickOnEditokBtn();
+                articleBase.WriteDec(body);
+                articleBase.WriteTags(tagExValue);
+                PreviewPage previewPage = articleBase.ClickOnPreviewBtn();
+                PostPage postPage = previewPage.ClickOnPublishBtn();
+                Assert.True(postPage.ValidatePostCreated("VIDEO:test article template"));
+            }
+        }
+
+        [TestFixture]
+        [Parallelizable]
+        public class Test3Class : BaseUi
+        {
+            [Test]
+            [Property("TestCaseId", "57")]
+            [Category("Sanity")]
+            [Category("Admin")]
+            [Category("MMNews")]
+            [Category("FullFlow")]
+            [Category("Pluralist")]
+            [Retry(2)]
+            public void FullFlow_TOPX()
+            {
+                Auth0LoginPage loginPage = new Auth0LoginPage(_browser);
+                NewsRoomPage newsRoomPage = loginPage.Login(_config.ConfigObject.Users.AdminUser);
+                EditorPage editorPage = newsRoomPage.ClickOnEditorBtn();
+                BsonArray tagExValue = _params["Tags"].AsBsonArray;
+                BsonArray titles = _params["Titles"].AsBsonArray;
+                string body = _params["Body"].ToString();
+                ListsTemplate listsTemplate = editorPage.ClickOnList();
+                listsTemplate.WriteTitle("VIDEO:test list template");
+                listsTemplate.DragImages();
+                listsTemplate.SetSubTitles(titles);
+                listsTemplate.SetBodyTextBoxs(body);
+                listsTemplate.WriteTags(tagExValue);
+                listsTemplate.ClickOnAscendingBtn();
+                listsTemplate.ClickOnDscBtn();
+                PreviewPage previewPage = listsTemplate.ClickOnPreviewBtn();
+                PostPage postPage = previewPage.ClickOnPublishBtn();
+                Assert.True(postPage.ValidatePostCreated("VIDEO:test list template"), "Post was not created");
+            }
+        }
+
+        [TestFixture]
+        [Parallelizable]
+        public class Test4Class : BaseUi
+        {
+            [Test]
+            [Property("TestCaseId", "58")]
+            [Category("Sanity")]
+            [Category("Admin")]
+            [Category("MMNews")]
+            [Category("FullFlow")]
+            [Category("Pluralist")]
+            [Retry(2)]
+            public void FullFlow_slideshow()
+            {
+                Auth0LoginPage loginPage = new Auth0LoginPage(_browser);
+                NewsRoomPage newsRoomPage = loginPage.Login(_config.ConfigObject.Users.AdminUser);
+                EditorPage editorPage = newsRoomPage.ClickOnEditorBtn();
+                BsonArray tagExValue = _params["Tags"].AsBsonArray;
+                BsonArray titles = _params["Titles"].AsBsonArray;
+                string body = _params["Body"].ToString();
+                SlideShowPage slideShow = editorPage.ClickOnSlideShow();
+                slideShow.WriteTitle("VIDEO:test slideshow template");
+                slideShow.DragAndDropImages();
+                slideShow.SetSubTitles(titles);
+                slideShow.SetBodyTextBoxsSlide(body);
+                slideShow.WriteTags(tagExValue);
+                slideShow.ClickOnAscendingBtn();
+                slideShow.ClickOnDscBtn();
+                PreviewPage previewPage = slideShow.ClickOnPreviewBtn();
+                PostPage postPage = previewPage.ClickOnPublishBtn();
+                Assert.True(postPage.ValidatePostCreated("VIDEO:test slideshow template"), "Post was not created");
+                var errors = postPage.ValidateComponents(_params["Components"].AsBsonArray);
+                Assert.True(string.IsNullOrEmpty(errors), errors);
+            }
+        }
+
+    }
+}
