9cca9d17d36d1b63d0608e7c876648c259d6de4a
liorshag <lior3545>
2018-02-14 18:47:45 +0200

    Added tests cases

---
+++ Automation/TestsFolder/AdminTestsFolder/EchoTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Automation.Helpersobjects;
+using Automation.PagesObjects;
+using Automation.PagesObjects.EchoFolder;
+using NUnit.Framework;
+
+namespace Automation.TestsFolder.AdminTestsFolder
+{
+    public class EchoTests
+    {
+        [TestFixture]
+        [Parallelizable]
+        public class Test1Class : BaseUi
+        {
+            [Test]
+            [Property("TestCaseId", "1")]
+            [Category("Sanity")]
+            [Category("Admin")]
+            [Category("Echo")]
+            [Category("AllBrands")]
+            [Retry(1)]
+            [Repeat(50)]
+            public void Echo_ValidatePostCreation()
+            {
+                Auth0LoginPage loginPage = new Auth0LoginPage(_browser);
+                NewsRoomPage newsRoomPage = loginPage.Login(_config.ConfigObject.Users.AdminUser);
+                PostCreatorEcho postCreatorEcho = new PostCreatorEcho(_browser);
+                string title = postCreatorEcho.CreatePost();
+                _browser.SwitchToFirstTab();
+                _browser.Refresh();
+                EchoPage echoPage = new EchoPage(_browser);
+                Assert.True(echoPage.ValidatePostCreation(title),$"post {title} is not shown on echo page.");
+            }
+        }
+    }
+}
