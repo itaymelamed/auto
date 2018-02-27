071c9ec3032656fe06af3ff1516d1731044a2bc3
itaymelamed <itaymelamed@ymail.com>
2018-02-27 14:28:23 +0200

    Ads,txt

--- Automation/PagesObjects/ExternalPagesobjects/AdsTxtValidatorPage.cs
+++ Automation/PagesObjects/ExternalPagesobjects/AdsTxtValidatorPage.cs
@@ -40,7 +40,7 @@ namespace Automation.PagesObjects.ExternalPagesobjects
                 {
                     errorsList = new List<string>();
                     errorsList = errors.ToList().Select(e => e.Text).ToList();
-                    errorsList.ForEach(e => errorsString += e + @"\n");
+                errorsList.ForEach(e => errorsString +=  "     *)" + e);
                 });
 
             return errorsString;
