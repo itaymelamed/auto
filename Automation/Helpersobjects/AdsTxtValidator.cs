d6e46eec8e3875d76a41e08e7a1e26fc0f8f3dfc
itaymelamed <itaymelamed@ymail.com>
2018-03-12 14:03:53 +0200

    ads -txt errors

--- Automation/Helpersobjects/AdsTxtValidator.cs
+++ Automation/Helpersobjects/AdsTxtValidator.cs
@@ -35,7 +35,7 @@ namespace Automation.Helpersobjects
         {
             var domainErrors = string.Empty;
             //_domains.ForEach(d => domainErrors += ValidateUrl(d)? "" : $"*) Error with url {d}.  {Environment.NewLine}");
-            _domains.ForEach(d => domainErrors += ValidateDomainFormat(d)? "" : $"*) Error with url {d}.  {Environment.NewLine}");
+            _domains.ForEach(d => domainErrors += ValidateDomainFormat(d)? "" : $"<div> Error with url {d}. </div>");
             return domainErrors;
         }
 
