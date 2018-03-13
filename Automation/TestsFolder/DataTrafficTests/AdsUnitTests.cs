b695c82a41a52c06832a4d66dad9cb496b296e83
itaymelamed <itaymelamed@ymail.com>
2018-03-13 11:28:06 +0200

    Fixed errors ads.txt

--- Automation/TestsFolder/DataTrafficTests/AdsUnitTests.cs
+++ Automation/TestsFolder/DataTrafficTests/AdsUnitTests.cs
@@ -158,7 +158,7 @@ namespace Automation.TestsFolder
 
                 ApiObject apiObject = new ApiObject();
                 if(!string.IsNullOrEmpty(errors))
-                    apiObject.GetRequestVoid($"http://{_config.Host}:32002/users/send?msg=<div><b><h2 style='color: red;'>{_config.SiteName} - following%20errors%20have%20found%20in%20ads.txt%20file:</h2></b></div>{errors}</br></br><img src='{_test.Result.ScreenShot}'></img>&to={Email}&subject={_config.SiteName} | Automation%20Google%20Ads.txt%20file%20-Tests%20failed");
+                    apiObject.GetRequestVoid($"http://{_config.Host}:32002/users/send?msg=<div><b><h2 style='color: red;'>{_config.SiteName} - The%20following%20errors%20were%20found%20in%20the%20ads.txt%20file:</h2></b></div>{errors}</br></br><img src='{_test.Result.ScreenShot}'></img>&to={Email}&subject={_config.SiteName} | Automation%20Google%20Ads.txt%20file%20-Tests%20failed");
                 Assert.True(string.IsNullOrEmpty(errors), errors);
             }
         }
