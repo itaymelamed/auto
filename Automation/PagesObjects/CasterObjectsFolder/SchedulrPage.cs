0c3afe08fb0f2c7e30f7cf76d69e8cb4ae465877
itaymelamed <itaymelamed@ymail.com>
2018-03-14 14:01:45 +0200

    Updated selenium class

--- Automation/PagesObjects/CasterObjectsFolder/SchedulrPage.cs
+++ Automation/PagesObjects/CasterObjectsFolder/SchedulrPage.cs
@@ -7,7 +7,6 @@ using MongoDB.Bson;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Globalization;
 
 namespace Automation.PagesObjects.CasterObjectsFolder
 {
