3e1d3321b5d9db9e1bcc5e0fec7b5bcb3ea55bda
yuvFri <yuval.f@90min.com>
2018-03-25 11:43:14 +0300

    removing tests from postpage Tests

--- Automation/TestsFolder/PostPagesFolder/PostPageTests.cs
+++ Automation/TestsFolder/PostPagesFolder/PostPageTests.cs
@@ -45,55 +45,6 @@ namespace Automation.TestsFolder.PostPagesFolder
                 Assert.True(counterRequest.Count() != 0, "A request to counter reads service was not sent.");
             }
         }
-        [TestFixture]
-        [Parallelizable]
-        public class Test2Class : BaseUi
-        {
-            [Test]
-            [Property("TestCaseId", "13")]
-            [Category("Sanity")]
-            [Category("Admin")]
-            [Category("PostPage")]
-            [Category("AllBrands")]
-            [Retry(2)]
-            public void PostPage_ValidateUiComponentsExistOnPage()
-            {
-                var exCurLanguage = "EN";
-                PostPage postPage = new PostPage(_browser);
-                bool result = postPage.ValidateCurrentLangaugeDropDown(exCurLanguage);
-                Assert.True(result);
-            }
-        }
-        [TestFixture]
-        [Parallelizable]
-        public class Test101Class : BaseUi
-        {
-            [Test]
-            [Property("TestCaseId", "101")]
-            [Category("Sanity")]
-            [Category("Admin")]
-            [Category("PostPage")]
-            [Category("12up")]
-            [Category("90Min")]
-            [Category("90MinIn")]
-            [Category("Ftb90")]
-            [Category("Floor8")]
 
-
-            [Retry(2)]
-            public void PostPage_ValidateUiComponentsExistOnPage()
-            {
-                //click on lang
-                // new header page
-                // curl lang = en
-                // curl =/ list
-                // curl langs
-
-                var languages = _params["languageDropDownNames"].AsBsonArray;
-                PostPage postPage = new PostPage(_browser);
-                bool result = postPage.ValidateLanguageDropDownLangauge(languages);
-                Assert.True(result);
-            }
         }
     }
-}
