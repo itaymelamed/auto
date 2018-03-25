0b6efb480dd66baefbaf184a9ed8f7536853c7e5
yuvFri <yuval.f@90min.com>
2018-03-25 11:45:58 +0300

    adding folder

--- Automation/TestsFolder/HeaderAndFooterFolder/HeaderTests.cs
+++ Automation/TestsFolder/HeaderAndFooterFolder/HeaderTests.cs
@@ -1,10 +1,63 @@
-﻿using System;
-namespace Automation.TestsFolder.HeaderAndFooterFolder
+﻿using Automation.PagesObjects;
+using MongoDB.Bson;
+using NUnit.Framework;
+using Automation.PagesObjects.ExternalPagesobjects;
+using System.Linq;
+
+namespace Automation.TestsFolder.PostPagesFolder
 {
+    [TestFixture]
     public class HeaderTests
     {
-        public HeaderTests()
+        [TestFixture]
+        [Parallelizable]
+        public class Test2Class : BaseUi
         {
+            [Test]
+            [Property("TestCaseId", "13")]
+            [Category("Sanity")]
+            [Category("Admin")]
+            [Category("PostPage")]
+            [Category("AllBrands")]
+            [Retry(2)]
+            public void PostPage_ValidateUiComponentsExistOnPage()
+            {
+                var exCurLanguage = "EN";
+                PostPage postPage = new PostPage(_browser);
+                bool result = postPage.ValidateCurrentLangaugeDropDown(exCurLanguage);
+                Assert.True(result);
+            }
+        }
+        [TestFixture]
+        [Parallelizable]
+        public class Test101Class : BaseUi
+        {
+            [Test]
+            [Property("TestCaseId", "101")]
+            [Category("Sanity")]
+            [Category("Admin")]
+            [Category("PostPage")]
+            [Category("12up")]
+            [Category("90Min")]
+            [Category("90MinIn")]
+            [Category("Ftb90")]
+            [Category("Floor8")]
+
+
+            [Retry(2)]
+            public void PostPage_ValidateUiComponentsExistOnPage()
+            {
+                //click on lang
+                // new header page
+                // curl lang = en
+                // curl =/ list
+                // curl langs
+
+                var languages = _params["languageDropDownNames"].AsBsonArray;
+                PostPage postPage = new PostPage(_browser);
+                bool result = postPage.ValidateLanguageDropDownLangauge(languages);
+                Assert.True(result);
+            }
         }
     }
 }
