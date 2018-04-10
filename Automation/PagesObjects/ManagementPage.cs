f80cd447d060a8cc677116ee795ac2c6bec8b66e
liorshag <liorshagan@gmail.com>
2018-04-10 14:20:07 +0300

    Added Eu tests

--- Automation/PagesObjects/ManagementPage.cs
+++ Automation/PagesObjects/ManagementPage.cs
@@ -1,13 +1,32 @@
-﻿using System;
-using Automation.BrowserFolder;
+﻿using Automation.BrowserFolder;
+using Automation.TestsFolder;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
 
 namespace Automation.PagesObjects
 {
     public class ManagementPage
     {
-        public ManagementPage(Browser broswer)
+        [FindsBy(How = How.CssSelector, Using = ".admin-tools__item-icon--editor")]
+        IWebElement EditorButton { get; set; }
+
+        protected BrowserHelper _browserHelper;
+        Browser _browser;
+        IWebDriver _driver;
+
+        public ManagementPage(Browser browser) 
         {
-            
+            _browser = browser;
+            _driver = browser.Driver;
+            _browserHelper = browser.BrowserHelper;
+            PageFactory.InitElements(_driver, this);
+        }
+
+        public void ClickOnEditorButton()
+        {
+            Base.MongoDb.UpdateSteps("Clicking on the editor button");
+            _browserHelper.WaitForElement(EditorButton, nameof(EditorButton));
+            _browserHelper.Click(EditorButton, nameof(EditorButton));
         }
     }
 }
--- Automation/TestsFolder/EITestsFolder/EditorTests.cs
+++ Automation/TestsFolder/EITestsFolder/EditorTests.cs
@@ -23,5 +23,23 @@ namespace Automation.TestsFolder.EITestsFolder
                 Assert.True(url == _config.Url + "/management", $"User was not login to the management page. Expected:{_config.Url}/management. but actual:{url}");
             }
         }
+
+        [TestFixture]
+        [Parallelizable]
+        public class EIArticleFullFlow : BaseUi
+        {
+            [Test]
+            [Property("TestCaseId", "144")]
+            [Category("EI")]
+            [Retry(1)]
+            public void EI_Article_FullFlow()
+            {
+                _browser.Navigate(_config.Url + "/management");
+                Auth0LoginPage loginPage = new Auth0LoginPage(_browser);
+                ManagementPage managementPage = loginPage.LoginEI(_config.ConfigObject.Users.AdminUser);
+
+
+            }
+        }
     }
 }
