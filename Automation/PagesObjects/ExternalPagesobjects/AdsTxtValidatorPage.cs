aa6369bb80afba87964821dc52c4f287cf13bdf6
itaymelamed <itaymelamed@ymail.com>
2018-04-12 11:34:10 +0300

    Removed using

--- Automation/PagesObjects/ExternalPagesobjects/AdsTxtValidatorPage.cs
+++ Automation/PagesObjects/ExternalPagesobjects/AdsTxtValidatorPage.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Automation.BrowserFolder;
-using Automation.TestsFolder;
 using MongoDB.Bson;
 using OpenQA.Selenium;
 using SeleniumExtras.PageObjects;
