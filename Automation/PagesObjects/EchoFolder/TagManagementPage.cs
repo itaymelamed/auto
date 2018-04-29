62b8133780bbf249296efba3e9fcb47ea20a5926
Lior Shagan <liorshagan@Liors-MacBook-Pro.local>
2018-04-29 17:51:35 +0300

    Added tests

---
+++ Automation/PagesObjects/EchoFolder/TagManagementPage.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Automation.BrowserFolder;
+using MongoDB.Bson;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+
+namespace Automation.PagesObjects.EchoFolder
+{
+    public class TagManagementPage : BaseObject
+    {
+
+        IWebElement TagsIcon => _browserHelper.FindElement(By.XPath("(//div[@class='sideNavContainer']//div//span)[2]"), "Tags");
+
+        IWebElement LangDropDown => FindElement(".ui.dropdown.distinct.selection");
+
+        IWebElement LangDropDownOpen => FindElement(".menu.transition.visible");
+
+        List<IWebElement> LanungeFilterList => FindElements("[role='option']");
+
+        IWebElement CreateTagBtn => FindElement(".ui.button.secondary");
+
+        IWebElement CreateTagWindow => FindElement(".ui.grey.basic.segment");
+
+        IWebElement TagNameField => FindElement("[placeholder='Tag Name']");
+
+        IWebElement SynonymsField => FindElement(".synonyms-input input");
+
+        IWebElement DoneBtn => FindElement(".ui.button.primary.medium");
+
+        IWebElement PopUpMessage => FindElement(".ui.black.basic.segment");
+
+        IWebElement SearchField => FindElement(".ui.icon.input input");
+
+        IWebElement SelectedTag => FindElement(".ui.label.chip");
+
+        IWebElement NoTagsFoundMsg => FindElement(".ui.brown.basic.segment");
+
+        IWebElement RemoveTagBtn => _browserHelper.FindElement(By.XPath("//span[contains(text(), 'REMOVE TAG')]"), "RemoveBtn");
+
+        IWebElement RemoveTagPopUpMsg => FindElement(".ui.tiny.modal.transition.visible.active");
+
+        IWebElement YesBtn_RemoveTagPopUp => _browserHelper.FindElement(By.XPath("//span[contains(text(), 'Yes')]"), "YesBtn");
+
+        public TagManagementPage(Browser browser)
+            :base(browser)
+        {
+        }
+
+        public void ClickOnTags()
+        {
+            _browserHelper.WaitForElement(() => TagsIcon, nameof(TagsIcon));
+            _browserHelper.Click(TagsIcon, nameof(TagsIcon));
+        }
+
+        public bool ValidateDropDown()
+        {
+            return _browserHelper.WaitForElement(() => LangDropDown, nameof(LangDropDown));
+        }
+
+        public void ClickOnLangBtn()
+        {
+            _browserHelper.WaitForElement(() => LangDropDown, nameof(LangDropDown));
+            _browserHelper.Click(LangDropDown, nameof(LangDropDown));
+        }
+
+        public List<string> GetLangList()
+        {
+            _browserHelper.WaitUntillTrue(() => LanungeFilterList.Count > 0);
+            return LanungeFilterList.Select(l => l.Text).ToList(); 
+        }
+
+        public bool ValidateLangList(BsonArray langnuges)
+        {
+            _browserHelper.WaitForElement(() => LangDropDownOpen, nameof(LangDropDownOpen));
+           var actualLangList = GetLangList();
+            return langnuges.Select(e => e.ToString()).SequenceEqual(actualLangList);
+        }
+
+        public bool ValidateCurrntLang(string langnuge)
+        {
+            _browserHelper.WaitForElement(() => LangDropDown, nameof(LangDropDown));
+            var actualDefaultLang = LangDropDown.Text;
+            return langnuge == actualDefaultLang;
+        }
+
+        public void ClickOnCreateBtn()
+        {
+            _browserHelper.WaitForElement(() => CreateTagBtn, nameof(CreateTagBtn));
+            _browserHelper.Click(CreateTagBtn,nameof(CreateTagBtn));
+        }
+
+        public string FillTheTagName()
+        {
+            _browserHelper.WaitForElement(() => CreateTagWindow, nameof(CreateTagWindow));
+            string tag = "NewTag" + new Random().Next(1,999999);
+            _browserHelper.SetText(TagNameField,tag);
+            return tag;
+        }
+
+        public void InsertSynonyms()
+        {
+            _browserHelper.WaitForElement(() => SynonymsField, nameof(SynonymsField));
+            _browserHelper.Click(SynonymsField, nameof(SynonymsField));
+            _browserHelper.SetText(SynonymsField, "NewSyn");
+            SynonymsField.SendKeys(Keys.Enter);
+        }
+
+        public void ClickOnDoneBtn()
+        {
+            _browserHelper.WaitForElement(() => DoneBtn, nameof(DoneBtn));
+            _browserHelper.Click(DoneBtn,nameof(DoneBtn));
+        }
+
+        public string GetPopUpText()
+        {
+            _browserHelper.WaitForElement(() => PopUpMessage, nameof(PopUpMessage));
+            var actualPopUpText = PopUpMessage.Text;
+            return actualPopUpText;
+        }
+
+        public void SearchForTagName(string tag)
+        {
+            _browserHelper.WaitForElement(() => SearchField, nameof(SearchField));
+            _browserHelper.Click(SearchField,nameof(SearchField));
+            _browserHelper.SetText(SearchField, tag);
+        }
+
+        public string GetTagText()
+        {
+            _browserHelper.WaitForElement(() => SelectedTag, nameof(SelectedTag));
+            var actualTagName = SelectedTag.Text;
+            return actualTagName;
+        }
+
+        public string GetNoTagsFoundMsg()
+        {
+            _browserHelper.WaitForElement(() => NoTagsFoundMsg, nameof(NoTagsFoundMsg));
+            var actualMsg = NoTagsFoundMsg.Text;
+            return actualMsg;
+        }
+
+        public void ClickOnTag(string tag)
+        {
+            _browserHelper.WaitForElement(() => SearchField, nameof(SearchField));
+            _browserHelper.Click(SearchField, nameof(SearchField));
+            _browserHelper.SetText(SearchField, tag);
+            _browserHelper.Click(SelectedTag, nameof(SelectedTag));
+        }
+
+        public void ClickOnTagNameField()
+        {
+            _browserHelper.WaitForElement(() => CreateTagWindow, nameof(CreateTagWindow));
+            _browserHelper.Click(TagNameField, nameof(TagNameField));
+        }
+
+        public void EditTag()
+        {
+            ClickOnTagNameField();
+            _browserHelper.SetText(TagNameField,"3");
+        }
+
+        public void ClickOnRemoveTagBtn()
+        {
+            _browserHelper.WaitForElement(() => CreateTagWindow,nameof(CreateTagWindow));
+            _browserHelper.Click(RemoveTagBtn,nameof(RemoveTagBtn));
+        }
+
+        public void ClickOnYesBtn()
+        {
+            _browserHelper.WaitForElement(() => RemoveTagPopUpMsg,nameof(RemoveTagPopUpMsg));
+            _browserHelper.Click(YesBtn_RemoveTagPopUp,nameof(YesBtn_RemoveTagPopUp));
+        }
+
+        public void RemoveTag(string tag)
+        {
+            ClickOnTag(tag);
+            ClickOnRemoveTagBtn();
+            ClickOnYesBtn();
+        }
+
+        public void ClickOnSelectedTag()
+        {
+            _browserHelper.WaitForElement(() => SelectedTag,nameof(SelectedTag));
+            _browserHelper.Click(SelectedTag,nameof(SelectedTag));
+        }
+    }
+}
---
+++ Automation/TestsFolder/AdminTestsFolder/TagManagementTests.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using Automation.PagesObjects;
+using NUnit.Framework;
+using Automation.PagesObjects.EchoFolder;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using MongoDB.Bson;
+
+namespace Automation.TestsFolder
+{
+    public class TagManagementTests
+    {
+        [TestFixture]
+        [Parallelizable]
+        public class EITest1 : BaseUi
+        {
+            [Test]
+            [Property("TestCaseId", "145")]
+            [Category("platform")]
+            [Retry(1)]
+            public void Login_Tags()
+            {
+                _browser.Navigate("http://" + _config.Env + "." + _config.ConfigObject.Echo);
+                Auth0LoginPage loginPage = new Auth0LoginPage(_browser);
+                NewsRoomPage newsRoomPage = loginPage.LoginNewsRoom(_config.ConfigObject.Users.AdminUser);
+                string url = _browser.GetUrl();
+                var expectedUrl = "http://" + _config.Env + "." + _config.ConfigObject.Echo;
+                Assert.True(url == expectedUrl, $"User was not login to the Tags management page. Expected:{expectedUrl} but actual:{url}");
+            }
+        }
+
+        [TestFixture]
+        [Parallelizable]
+        public class EITest2 : BaseUi
+        {
+            [Test]
+            [Property("TestCaseId", "146")]
+            [Category("platform")]
+            [Retry(1)]
+            public void Tags_ValidateLangDropDownList()
+            {
+                _browser.Navigate("http://" + _config.Env + "." + _config.ConfigObject.Echo);
+                Auth0LoginPage loginPage = new Auth0LoginPage(_browser);
+                NewsRoomPage newsRoomPage = loginPage.LoginNewsRoom(_config.ConfigObject.Users.AdminUser);
+                TagManagementPage tagManagementPage = new TagManagementPage(_browser);
+                Assert.True(tagManagementPage.ValidateDropDown());
+            }
+        }
+
+
+        [TestFixture]
+        [Parallelizable]
+        public class EITest3 : BaseUi
+        {
+            [Test]
+            [Property("TestCaseId", "147")]
+            [Category("platform")]
+            [Retry(1)]
+            public void Tags_ValidateLangDropDownListOpen()
+            {
+                _browser.Navigate("http://" + _config.Env + "." + _config.ConfigObject.Echo);
+                Auth0LoginPage loginPage = new Auth0LoginPage(_browser);
+                NewsRoomPage newsRoomPage = loginPage.LoginNewsRoom(_config.ConfigObject.Users.AdminUser);
+                TagManagementPage tagManagementPage = new TagManagementPage(_browser);
+                Assert.True(tagManagementPage.ValidateDropDown());
+            }
+        }
+
+        [TestFixture]
+        [Parallelizable]
+        public class EITest4 : BaseUi
+        {
+            [Test]
+            [Property("TestCaseId", "166")]
+            [Category("platform")]
+            [Retry(1)]
+            public void platform_Tags_GetLangList()
+            {
+                _browser.Navigate(_config.ConfigObject.Echo);
+                BsonArray languagesList = _params["langnuges"].AsBsonArray;
+                Auth0LoginPage loginPage = new Auth0LoginPage(_browser);
+                NewsRoomPage newsRoomPage = loginPage.LoginNewsRoom(_config.ConfigObject.Users.AdminUser);
+                TagManagementPage tagManagementPage = new TagManagementPage(_browser);
+                tagManagementPage.ClickOnTags();
+                tagManagementPage.ClickOnLangBtn();
+                Assert.True(tagManagementPage.ValidateLangList(languagesList));
+            }
+        }
+
+        [TestFixture]
+        [Parallelizable]
+        public class EITest5 : BaseUi
+        {
+            [Test]
+            [Property("TestCaseId", "167")]
+            [Category("platform")]
+            [Retry(1)]
+            public void platform_Tags_ValidateDefualtLang()
+            {
+                _browser.Navigate(_config.ConfigObject.Echo);
+                string languagesList = _params["langnuge"].ToString();
+                Auth0LoginPage loginPage = new Auth0LoginPage(_browser);
+                NewsRoomPage newsRoomPage = loginPage.LoginNewsRoom(_config.ConfigObject.Users.AdminUser);
+                TagManagementPage tagManagementPage = new TagManagementPage(_browser);
+                tagManagementPage.ClickOnTags();
+                Assert.True(tagManagementPage.ValidateCurrntLang(languagesList));
+            }
+        }
+
+        [TestFixture]
+        [Parallelizable]
+        public class EITest6 : BaseUi
+        {
+            [Test]
+            [Property("TestCaseId", "168")]
+            [Category("platform")]
+            [Retry(1)]
+            public void platform_Tags_CreateNewTag_ValidateSucMsg()
+            {
+                _browser.Navigate(_config.ConfigObject.Echo);
+                string popupMessageText = _params["popupText"].ToString();
+                Auth0LoginPage loginPage = new Auth0LoginPage(_browser);
+                NewsRoomPage newsRoomPage = loginPage.LoginNewsRoom(_config.ConfigObject.Users.AdminUser);
+                TagManagementPage tagManagementPage = new TagManagementPage(_browser);
+                tagManagementPage.ClickOnTags();
+                tagManagementPage.ClickOnCreateBtn();
+                string tagName = tagManagementPage.FillTheTagName();
+                popupMessageText = popupMessageText.Replace("<tag>", tagName);
+                tagManagementPage.InsertSynonyms();
+                tagManagementPage.ClickOnDoneBtn();
+                var actualResult = tagManagementPage.GetPopUpText().Replace("DISMISS", "").Replace("\n", "");
+                var expectedResult = popupMessageText;
+                Assert.AreEqual(expectedResult, actualResult, $"Expected text was: {expectedResult} Actual: {actualResult}");
+            }
+        }
+
+        [TestFixture]
+        [Parallelizable]
+        public class EITest7 : BaseUi
+        {
+            [Test]
+            [Property("TestCaseId", "170")]
+            [Category("platform")]
+            [Retry(1)]
+            public void platform_Tags_CreateNewTag()
+            {
+                _browser.Navigate(_config.ConfigObject.Echo);
+                Auth0LoginPage loginPage = new Auth0LoginPage(_browser);
+                NewsRoomPage newsRoomPage = loginPage.LoginNewsRoom(_config.ConfigObject.Users.AdminUser);
+                TagManagementPage tagManagementPage = new TagManagementPage(_browser);
+                tagManagementPage.ClickOnTags();
+                tagManagementPage.ClickOnCreateBtn();
+                string tagName = tagManagementPage.FillTheTagName();
+                tagManagementPage.InsertSynonyms();
+                tagManagementPage.ClickOnDoneBtn();
+                tagManagementPage.SearchForTagName(tagName);
+                var actualresult = tagManagementPage.GetTagText();
+                var expctedResult = tagName;
+                Assert.AreEqual(expctedResult, actualresult, $"Expected tag name was: {expctedResult} Actual: {actualresult}");
+            }
+        }
+
+            [TestFixture]
+            [Parallelizable]
+            public class EITest8 : BaseUi
+            {
+                [Test]
+                [Property("TestCaseId", "171")]
+                [Category("platform")]
+                [Retry(1)]
+                public void platform_Tags_SearchforNonExistingTag()
+                {
+                    _browser.Navigate(_config.ConfigObject.Echo);
+                    string NonExTag = _params["tag"].ToString();
+                    Auth0LoginPage loginPage = new Auth0LoginPage(_browser);
+                    NewsRoomPage newsRoomPage = loginPage.LoginNewsRoom(_config.ConfigObject.Users.AdminUser);
+                    TagManagementPage tagManagementPage = new TagManagementPage(_browser);
+                    tagManagementPage.ClickOnTags();
+                    tagManagementPage.SearchForTagName(NonExTag);
+                    string actualMsg = tagManagementPage.GetNoTagsFoundMsg();
+                    string expectedMsg = "No tags were found matching the search term.  ADD AS NEW TAG";
+                    Assert.AreEqual(expectedMsg, actualMsg, $"Expected tag name was: {expectedMsg} Actual: {actualMsg}");
+                }
+            }
+
+                [TestFixture]
+                [Parallelizable]
+                public class EITest9 : BaseUi
+                {
+                    [Test]
+                    [Property("TestCaseId", "172")]
+                    [Category("platform")]
+                    [Retry(1)]
+                    public void platform_Tags_EditTag()
+                    {
+                 _browser.Navigate(_config.ConfigObject.Echo);
+                Auth0LoginPage loginPage = new Auth0LoginPage(_browser);
+                NewsRoomPage newsRoomPage = loginPage.LoginNewsRoom(_config.ConfigObject.Users.AdminUser);
+                TagManagementPage tagManagementPage = new TagManagementPage(_browser);
+                tagManagementPage.ClickOnTags();
+                tagManagementPage.ClickOnCreateBtn();
+                string tagName = tagManagementPage.FillTheTagName();
+                tagManagementPage.InsertSynonyms();
+                tagManagementPage.ClickOnDoneBtn();
+                tagManagementPage.SearchForTagName(tagName);
+                tagManagementPage.ClickOnTag(tagName);
+                tagManagementPage.ClickOnTagNameField();
+                tagManagementPage.EditTag();
+                string actualTagName = tagManagementPage.GetTagText();
+                string expectedTagName = tagName + 3;
+                Assert.AreEqual(expectedTagName, actualTagName, $"Expected tag name was: {expectedTagName} Actual: {actualTagName}");
+
+                    }
+               }
+
+        [TestFixture]
+        [Parallelizable]
+        public class EITest10 : BaseUi
+        {
+            [Test]
+            [Property("TestCaseId", "173")]
+            [Category("platform")]
+            [Retry(1)]
+            public void platform_Tags_RemoveTag()
+            {
+                _browser.Navigate(_config.ConfigObject.Echo);
+                Auth0LoginPage loginPage = new Auth0LoginPage(_browser);
+                NewsRoomPage newsRoomPage = loginPage.LoginNewsRoom(_config.ConfigObject.Users.AdminUser);
+                TagManagementPage tagManagementPage = new TagManagementPage(_browser);
+                tagManagementPage.ClickOnTags();
+                tagManagementPage.ClickOnCreateBtn();
+                string tagName = tagManagementPage.FillTheTagName();
+                tagManagementPage.InsertSynonyms();
+                tagManagementPage.ClickOnDoneBtn();
+                tagManagementPage.SearchForTagName(tagName);
+                tagManagementPage.ClickOnSelectedTag();
+                tagManagementPage.ClickOnRemoveTagBtn();
+
+
+            }
+        }
+ 
+    }
+}
